23acdb92ee10a815e90a8ae94f35df41d889c420
Lim_Kong <Inf_Kong@163.com>
2024-10-03 01:53:55 +0800

    更新

--- RiemannianGeometry/mainresult.lean
+++ RiemannianGeometry/mainresult.lean
@@ -1,9 +1,17 @@
 import Mathlib.Data.Finsupp.Defs
+import Mathlib.Data.Set.Lattice
+import Mathlib.Data.Nat.Prime.Basic
 
 open Function
 
+def evens : Set ℕ :=
+  { n | Even n }
+
+lemma luo_lemma (h : s ⊆ t) : s ∩ u ⊆ t ∩ u :=
+  fun x ⟨xs, xu⟩ ↦ ⟨h xs, xu⟩
+  
 /-- `Finsupp.mapRange` of a surjective function is surjective. -/
-lemma luo_lemma {α M N : Type*} [Zero M] [Zero N] (f : M → N) (hf : f 0 = 0)
+lemma Asuka_Takatsu_theorem {α M N : Type*} [Zero M] [Zero N] (f : M → N) (hf : f 0 = 0)
     (hs : Surjective f) : Surjective (Finsupp.mapRange (α := α) f hf) := by
   classical
   let g (n : N) : M := if n = 0 then 0 else surjInv hs n
@@ -19,3 +27,4 @@ lemma luo_lemma {α M N : Type*} [Zero M] [Zero N] (f : M → N) (hf : f 0 = 0)
   rw [← Finsupp.mapRange_comp (h := hfg)]
   convert Finsupp.mapRange_id F
   convert this.id
+
